86dda9b7474f61a95f15cfac56bc7a8e035e4db0
CoinHub <sybca21@gmail.com>
2023-08-01 00:33:45 +0530

    Commit UI-1.

--- app/src/main/java/com/tufelmalik/quizapp/ui/RegistationActivity.kt
+++ app/src/main/java/com/tufelmalik/quizapp/ui/RegistationActivity.kt
@@ -3,10 +3,15 @@ package com.tufelmalik.quizapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tufelmalik.quizapp.R
+import com.tufelmalik.quizapp.databinding.ActivityRegistationBinding
 
 class RegistationActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityRegistationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registation)
+        binding = ActivityRegistationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
--- app/src/main/java/com/tufelmalik/quizapp/ui/StartActivity.kt
+++ app/src/main/java/com/tufelmalik/quizapp/ui/StartActivity.kt
@@ -1,12 +1,25 @@
 package com.tufelmalik.quizapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tufelmalik.quizapp.R
+import com.tufelmalik.quizapp.databinding.ActivitySplashBinding
+import com.tufelmalik.quizapp.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGotoSignIn.setOnClickListener {
+            startActivity(Intent(this@StartActivity,LoginActivity::class.java))
+        }
+
+        binding.txtGotoReg.setOnClickListener {
+            startActivity(Intent(this@StartActivity,RegistationActivity::class.java))
+        }
     }
 }
