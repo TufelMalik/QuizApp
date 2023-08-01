b929821097452cdb5d16657abbeeb99ad990fb69
TufelMalik <mtufel21@gmail.com>
2023-08-01 09:53:11 +0530

    first college commit.

--- app/src/main/java/com/tufelmalik/quizapp/ui/LoginActivity.kt
+++ app/src/main/java/com/tufelmalik/quizapp/ui/LoginActivity.kt
@@ -3,10 +3,16 @@ package com.tufelmalik.quizapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tufelmalik.quizapp.R
+import com.tufelmalik.quizapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+        private lateinit var binding : ActivityLoginBinding
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+            binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+            //First change  from the college
+
     }
 }
