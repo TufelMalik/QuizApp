4451c63c4dc221cd4d5196e80ab78000e5537a96
TufelMalik <mtufel21@gmail.com>
2023-08-01 11:00:59 +0530

    commit from college UI-2.

--- app/src/main/java/com/tufelmalik/quizapp/ui/MainActivity.kt
+++ app/src/main/java/com/tufelmalik/quizapp/ui/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.tufelmalik.quizapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.ColorSpace.Model
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 import com.tufelmalik.quizapp.R
+import com.tufelmalik.quizapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        binding.obottomNavigation.add(MeowBottomNavigation.Model(1, R.drawable.ic_email))
+//        binding.bottomNavigation.add(MeowBottomNavigation.Model(2, R.drawable.ic_email))
+//        binding.bottomNavigation.add(MeowBottomNavigation.Model(3, R.drawable.ic_email))
     }
 }
